daa6d6bbbf70ac3a539836afd90592a596045abb
David J. Hammaker <davidjosh93@gmail.com>
2019-11-08 23:44:50 -0500

    Create base-level app design

--- src/components/home.js
+++ src/components/home.js
@@ -6,14 +6,13 @@ export default function Home() {
     <div className="d-flex align-items-center min-vh-100">
       <div className="container text-center">
         <h1 className="color-very-light font-weight-bold mb-5">
-          <span className="title-style">VOT</span>E<span className="title-style"> PREDIC</span>T</h1>
-        <div >
-          <h6 className="row">
-            <div className="col-sm-4 p-4"><a className="text-muted text-decoration-none" href="/">Sign&nbsp;Up</a></div>
-            <div className="col-sm-4 p-4"><a className="text-muted text-decoration-none" href="/login">Log&nbsp;In</a></div>
-            <div className="col-sm-4 p-4"><a className="text-muted text-decoration-none" href="/">Where&nbsp;am&nbsp;I?</a></div>
-          </h6>
-        </div>
+          <span className="title-style">VOT</span>E<span className="title-style"> PREDIC</span>T
+        </h1>
+        <h6 className="row">
+          <div className="col-sm-4 p-4"><a className="text-muted text-decoration-none" href="/">Sign&nbsp;Up</a></div>
+          <div className="col-sm-4 p-4"><a className="text-muted text-decoration-none" href="/login">Log&nbsp;In</a></div>
+          <div className="col-sm-4 p-4"><a className="text-muted text-decoration-none" href="/">Where&nbsp;am&nbsp;I?</a></div>
+        </h6>
       </div>
     </div>
   );
--- src/components/login.js
+++ src/components/login.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axiosUtils from './utils/axiosUtils';
 
 
 export default function Login() {
   return (
-    <button
-      onClick={() => {axiosUtils.login('username', 'password');}}
-    >
-      Click Me
-    </button>
+    <div className="d-flex align-items-center min-vh-100">
+      <div className="container">
+        <h3 className="color-very-light font-weight-bold text-center mb-3">
+          Log In
+        </h3>
+        <div className="row">
+          <div className="col-md-4" />
+          <form onSubmit="{ this.handleSubmit }" className="p-4 col-md-4">
+            <div message="{ message }" />
+            <small></small>
+            <div className="form-group mb-4">
+              <label className="text-muted">Username:</label>
+              <input
+                type="text"
+                value="{ this.state.value }"
+                onChange="{ this.handleUsernameChange }"
+                className="form-control" />
+            </div>
+            <div className="form-group mb-4">
+              <label className="text-muted">Password:</label>
+              <input
+                type="password"
+                value="{ this.state.value }"
+                onChange="{ this.handlePasswordChange }"
+                className="form-control" />
+            </div>
+            <div className="d-flex justify-content-end">
+              <input type="submit" value="Submit" className="btn btn-secondary mt-2" />
+            </div>
+          </form>
+          <div className="col-md-4" />
+        </div>
+      </div>
+    </div>
   )
 }
