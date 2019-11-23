c0c8fb19ebf2b9e6573a3854c96c41463eee7252
David J. Hammaker <davidjosh93@gmail.com>
2019-11-22 23:42:52 -0500

    Fix mobile views

--- src/components/signup.js
+++ src/components/signup.js
@@ -90,7 +90,7 @@ export default function SignUp() {
   return (
     <>
       <NavBar />
-      <div className="d-flex align-items-center min-vh-100 centralized" id="signUp">
+      <div className="d-flex align-items-center min-vh-100 centralized">
         <div className="container">
           <Message />
           <h3 className="color-very-light font-weight-bold text-center mb-3">
